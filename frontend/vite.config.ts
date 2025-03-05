62a48bf0919280a6b865145d0d72f51112cebe2a
d0phamin3 <sizow123@gmail.com>
2025-03-05 10:37:41 +0300

    add some dirs to cfg

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 			"@assets": path.resolve(__dirname, "./src/assets"),
 			"@components": path.resolve(__dirname, "./src/components"),
+			"@hooks": path.resolve(__dirname, "./src/hooks"),
 			"@layouts": path.resolve(__dirname, "./src/layout"),
 			"@pages": path.resolve(__dirname, "./src/pages"),
 			"@store": path.resolve(__dirname, "./src/store"),
 			"@styles": path.resolve(__dirname, "./src/styles"),
+			"@utils": path.resolve(__dirname, "./src/utils"),
 		},
 	},
 })
